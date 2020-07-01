2b710cc9be34e4502a4e2318edab4743e568f432
Mohammad Kabir <mgkabir@gmail.com>
2020-07-01 22:47:35 +1000

    gcd

--- src/main/java/io/kabir/maths/MathUtils.java
+++ src/main/java/io/kabir/maths/MathUtils.java
@@ -5,4 +5,14 @@ public class MathUtils {
     public int add(int a, int b){
         return a+b;
     }
+
+    public int gcd(int[] numbers){
+        return getGCD(numbers[0],numbers[1]);
+    }
+
+    private int getGCD(int num1, int num2){
+        if(num2 == 0) return num1;
+
+        return getGCD(num2, num1 % num2);
+    }
 }
--- src/test/java/io/kabir/maths/MathUtilsTest.java
+++ src/test/java/io/kabir/maths/MathUtilsTest.java
@@ -24,4 +24,14 @@ class MathUtilsTest {
         assertEquals(5, mUtils.add(2,3));
         assertNotEquals(5, mUtils.add(7,3));
     }
+
+    @Test
+    void gcd() {
+        int[] numArray1 = {2,3};
+        int[] numArray2 = {20,30};
+        int[] numArray3 = {3,9};
+        assertEquals(1, mUtils.gcd(numArray1));
+        assertEquals(10, mUtils.gcd(numArray2));
+        assertEquals(3, mUtils.gcd(numArray3));
+    }
 }
