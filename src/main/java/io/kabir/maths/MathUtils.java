248dbd5f255ec8fa1a2d4923c3554e672e74b8d2
Mohammad Kabir <mgkabir@gmail.com>
2020-07-02 21:57:20 +1000

    gcd function extended for n numbers

--- src/main/java/io/kabir/maths/MathUtils.java
+++ src/main/java/io/kabir/maths/MathUtils.java
@@ -1,18 +1,39 @@
 package io.kabir.maths;
 
+import java.util.stream.IntStream;
+
 public class MathUtils {
 
-    public int add(int a, int b){
-        return a+b;
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public int gcd(int[] numbers){
-        return getGCD(numbers[0],numbers[1]);
+    public int gcd(int[] numbers) {
+        int gcd = 0;
+        if (numbers.length < 2) throw new IllegalArgumentException("At least 2 numbers required");
+        boolean containsZero = IntStream.of(numbers).anyMatch(n -> n == 0);
+        if (containsZero) throw new IllegalArgumentException("Non Zero Value");
+        if (numbers.length == 2) {
+            gcd = getGCD(numbers[0], numbers[1]);
+        } else {
+            int num1 = numbers[0];
+            int num2 = numbers[1];
+            gcd = getGCD(num1, num2);
+            for (int idx = 2; idx < numbers.length; idx++) {
+                num1 = gcd;
+                num2 = numbers[idx];
+                gcd = getGCD(num1, num2);
+            }
+        }
+        return gcd;
     }
 
-    private int getGCD(int num1, int num2){
-        if(num2 == 0) return num1;
-
+    private int getGCD(int num1, int num2) {
+        System.out.println("num1 : " + num1 + " num2 : " + num2);
+        if (num2 == 0) {
+            System.out.println(" == returning == " + num1);
+            return num1;
+        }
         return getGCD(num2, num1 % num2);
     }
 }
--- src/test/java/io/kabir/maths/MathUtilsTest.java
+++ src/test/java/io/kabir/maths/MathUtilsTest.java
@@ -28,10 +28,18 @@ class MathUtilsTest {
     @Test
     void gcd() {
         int[] numArray1 = {2,3};
-        int[] numArray2 = {20,30};
+        int[] numArray2 = {20,40,60,70};
         int[] numArray3 = {3,9};
+        int[] numArray4 = {3,9,6};
+        int[] numArray5 = {3,9,6,12,15};
+        int[] numArray0 = {};
+        int[] numArrayWith0 = {2,6,10,0,20};
         assertEquals(1, mUtils.gcd(numArray1));
         assertEquals(10, mUtils.gcd(numArray2));
         assertEquals(3, mUtils.gcd(numArray3));
+        assertEquals(3, mUtils.gcd(numArray4));
+        assertEquals(3, mUtils.gcd(numArray5));
+        assertThrows(IllegalArgumentException.class,()->mUtils.gcd(numArray0));
+        assertThrows(IllegalArgumentException.class,()->mUtils.gcd(numArrayWith0));
     }
 }
